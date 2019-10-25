ab150dd5b4182bb71f7bee7b5f400ad22bd31783
Will Tackett <will.s.tackett@gmail.com>
2019-10-25 11:19:50 -0400

    update subject name generation

--- list_subs.sh
+++ list_subs.sh
@@ -2,12 +2,16 @@
 # 
 # write a text file with a list of names
 #
+# e.g. ./list_subs.sh 50 60 subs.txt 'P' will produce a file subs.txt with P50, P51, P52...P60
+#
+# If file already exists, calling list_subs.sh will overwrite anything that's there
+#
 
-> controls.txt
+> $3
 
 min=$1;
 max=$2;
 while [ "$min" -le "$max" ]; do
-	echo "C$min" >> subs.txt
+	echo "$4$min" >> $3
 	min=`expr "$min" + 1`;
 done
