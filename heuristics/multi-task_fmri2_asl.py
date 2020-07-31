85fc658fe5f4c47d83db66124f5869130d73c638
Will Tackett <will.s.tackett@gmail.com>
2020-07-31 11:43:25 -0400

    heuristic update test

--- heuristics/multi-task_fmri2_asl.py
+++ heuristics/multi-task_fmri2_asl.py
@@ -7,55 +7,65 @@ def create_key(template, outtype=('nii.gz',), annotation_classes=None):
 
 # structurals
 t1w = create_key(
-    'sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_T1w')
+    'sub-{subject}/ses-{session}/anat/sub-{subject}_{session}_T1w')
 t2w = create_key(
-     'sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_T2w')
+     'sub-{subject}/ses-{session}/anat/sub-{subject}_{session}_T2w')
 t2_2d = create_key(
-     'sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_acq-2D_T2w')
+     'sub-{subject}/ses-{session}/anat/sub-{subject}_{session}_acq-2D_T2w')
+t2_cor = create_key(
+     'sub-{subject}/ses-{session}/anat/sub-{subject}_{session}_acq-coronal_T2w')
 flair = create_key(
-    'sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_FLAIR')
+    'sub-{subject}/ses-{session}/anat/sub-{subject}_{session}_FLAIR')
 flair2 = create_key(
-    'sub-{subject}/ses-{session}/anat/sub-{subject}_ses-{session}_acq-3D_FLAIR')
+    'sub-{subject}/ses-{session}/anat/sub-{subject}_{session}_acq-3D_FLAIR')
+tof1 = create_key(
+    'sub-{subject}/ses-{session}/anat/sub-{subject}_{session}_angio')
+tof2 = create_key(
+    'sub-{subject}/ses-{session}/anat/sub-{subject}_{session}_acq-cor_angio')
+tof3 = create_key(
+    'sub-{subject}/ses-{session}/anat/sub-{subject}_{session}_acq-sag_angio')
 
 # task fMRI
 object_run1 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-object_run-01_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-object_run-01_bold')
 object_run2 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-object_run-02_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-object_run-02_bold')
 rhyme_run1 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-rhyme_run-01_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-rhyme_run-01_bold')
 rhyme_run2 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-rhyme_run-02_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-rhyme_run-02_bold')
 scenemem_run1 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-scenemem_run-01_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-scenemem_run-01_bold')
 scenemem_run2 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-scenemem_run-02_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-scenemem_run-02_bold')
 sentence_run1 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-sentence_run-01_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-sentence_run-01_bold')
 sentence_run2 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-sentence_run-02_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-sentence_run-02_bold')
 wordgen_run1 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-wordgen_run-01_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-wordgen_run-01_bold')
 wordgen_run2 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-wordgen_run-02_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-wordgen_run-02_bold')
 binder_run1 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-binder_run-01_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-binder_run-01_bold')
 binder_run2 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-binder_run-02_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-binder_run-02_bold')
 verbgen_run1 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-verbgen_run-01_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-verbgen_run-01_bold')
 verbgen_run2 = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-verbgen_run-02_bold')
-rest = create_key(
-    'sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_task-rest_bold')
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-verbgen_run-02_bold')
+rest_run1 = create_key(
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-rest_run-01_bold')
+rest_run2 = create_key(
+    'sub-{subject}/ses-{session}/func/sub-{subject}_{session}_task-rest_run-02_bold')
 
 # ASL scans
 asl = create_key(
-     'sub-{subject}/ses-{session}/perf/sub-{subject}_ses-{session}_asl')
+     'sub-{subject}/ses-{session}/perf/sub-{subject}_{session}_asl')
 m0 = create_key(
-    'sub-{subject}/ses-{session}/perf/sub-{subject}_ses-{session}_m0scan')
+    'sub-{subject}/ses-{session}/perf/sub-{subject}_{session}_m0scan')
 mean_perf = create_key(
-    'sub-{subject}/ses-{session}/perf/sub-{subject}_ses-{session}_CBF')
+    'sub-{subject}/ses-{session}/perf/sub-{subject}_{session}_mean-perfusion')
 
 # Diffusion
 dwi = create_key(
@@ -63,25 +73,25 @@ dwi = create_key(
 
 # Field maps
 b0_mag = create_key(
-   'sub-{subject}/ses-{session}/fmap/sub-{subject}_ses-{session}_magnitude{item}')
+   'sub-{subject}/ses-{session}/fmap/sub-{subject}_{session}_magnitude{item}')
 b0_phase = create_key(
-   'sub-{subject}/ses-{session}/fmap/sub-{subject}_ses-{session}_phasediff')
+   'sub-{subject}/ses-{session}/fmap/sub-{subject}_{session}_phasediff')
 pe_rev = create_key(
-    'sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-multishell_dir-AP_epi')
+    'sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-multishell_dir-j_epi')
 bold_tu = create_key(
-    'sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-bold_dir-AP_epi')
+    'sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-bold_dir-j_epi')
 
 
 def infotodict(seqinfo):
 
     last_run = len(seqinfo)
 
-    info = {t1w:[], t2w:[], t2_2d:[], flair:[], flair2:[], pe_rev: [],dwi:[],
-            object_run1: [], object_run2: [], rhyme_run1: [], rhyme_run2: [],
-            scenemem_run1: [], scenemem_run2: [], sentence_run1: [], sentence_run2: [],
-            wordgen_run1: [], wordgen_run2: [], binder_run1: [], binder_run2:[],
-            verbgen_run1: [], verbgen_run2: [], rest: [], bold_tu: [], asl: [],
-            m0: [], mean_perf: [], b0_phase: [], b0_mag: []}
+    info = {t1w:[], t2w:[], t2_2d:[], t2_cor:[],flair:[], flair2:[], tof1: [], tof2: [], tof3: [],
+            pe_rev: [],dwi:[],object_run1: [], object_run2: [], rhyme_run1: [],
+            rhyme_run2: [],scenemem_run1: [], scenemem_run2: [], sentence_run1: [],
+            sentence_run2: [],wordgen_run1: [], wordgen_run2: [], binder_run1: [],
+            binder_run2:[],verbgen_run1: [], verbgen_run2: [], rest_run1: [], rest_run2: [],
+            bold_tu: [],asl: [], m0: [], mean_perf: [], b0_phase: [], b0_mag: []}
 
 # sometimes patients struggle with a task the first time around (or something
 # else goes wrong and often some tasks are repeated. This function accomodates
@@ -105,10 +115,20 @@ def infotodict(seqinfo):
             get_series(t2w,s)
         elif "t2_2d" in protocol:
             get_series(t2_2d, s)
+        elif "t2_tse_coronal" in protocol:
+            get_series(t2_cor, s)
         elif "tra_flair" in protocol:
             get_series(flair,s)
         elif "flair" in protocol and "3d" in protocol:
             get_series(flair2,s)
+
+        elif "tof" in protocol:
+            if "COR" in s.series_description:
+                get_series(tof2,s)
+            elif "SAG" in s.series_description:
+                get_series(tof3,s)
+            else:
+                get_series(tof1,s)
 
         elif "topup" in protocol and "BOLD" not in s.series_description:
             get_series(pe_rev, s)
@@ -129,8 +149,8 @@ def infotodict(seqinfo):
             get_both_series(binder_run1, binder_run2,s)
         elif "verbgen" in protocol:
             get_both_series(verbgen_run1, verbgen_run2,s)
-        elif "restBOLD" in protocol and "asl" not in protocol:
-            get_series(rest,s)
+        elif "restBOLD" in s.series_description and "asl" not in protocol:
+            get_both_series(rest_run1, rest_run2, s)
         elif "topup" in protocol and "MULTISHELL" not in s.series_description:
             get_series(bold_tu, s)
 
@@ -139,6 +159,8 @@ def infotodict(seqinfo):
                 get_series(asl,s)
             elif s.series_description.endswith("_M0"):
                 get_series(m0,s)
+            elif s.series_description.endswith("_MeanPerf"):
+                get_series(mean_perf,s)
 
         elif "b0map" in protocol:
                 if "P" in s.image_type:
@@ -156,13 +178,133 @@ def AttachToSession():
     data = '\n'.join(data)
     data = 'volume_type\n' + data # the data is now a string; perfect!
 
-    output_file = {
-      'name': '{subject}_{session}_aslcontext.tsv',
+    attachment1 = {
+      'name': '{subject}/{session}/perf/{subject}_{session}_aslcontext.tsv',
       'data': data,
       'type': 'text/tab-separated-values'
     }
 
-    return output_file
+    return attachment1
+
+def AttachToProject():
+    import pandas as pd
+    object_data = {'onset': [0, 18, 36, 54, 72, 90, 108, 126, 144, 162, 180, 198, 16, 234, 252, 270],
+                   'duration': [18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18],
+                   'weight': [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+                   'trial_type':
+                  ['baseline',
+                   'stimulus',
+                   'baseline',
+                   'stimulus',
+                   'baseline',
+                   'stimulus',
+                   'baseline',
+                   'stimulus',
+                   'baseline',
+                   'stimulus',
+                   'baseline',
+                   'stimulus',
+                   'baseline',
+                   'stimulus',
+                   'baseline',
+                   'stimulus']}
+    o_df = pd.DataFrame(object_data, columns = ['onset', 'duration','trial_type'])
+    object = {
+        'name': 'task-object_events.tsv',
+        'data': o_df.to_csv(index=False, sep='\t'),
+        'type': 'text/tab-separated-values'
+    }
+
+    rhyme_data ={'onset': [0, 30, 60, 90, 120, 150, 180, 210, 240, 270],
+                'duration': [30, 30, 30, 30, 30, 30, 30, 30, 30, 30],
+                'weight': [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+                'trial_type':
+                ['baseline',
+                 'stimulus',
+                 'baseline',
+                 'stimulus',
+                 'baseline',
+                 'stimulus',
+                 'baseline',
+                 'stimulus',
+                 'baseline',
+                 'stimulus']}
+    r_df = pd.DataFrame(rhyme_data, columns = ['onset', 'duration','trial_type'])
+    rhyme = {
+        'name': 'task-rhyme_events.tsv',
+        'data': r_df.to_csv(index=False, sep='\t'),
+        'type': 'text/tab-separated-values'
+    }
+
+    scenemem_data = {'onset': [0, 36, 72, 108, 144, 180, 216, 252, 288, 324, 360, 396],
+                     'duration': [36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36],
+                     'weight': [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+                     'trial_type':
+                     ['baseline',
+                      'stimulus',
+                      'baseline',
+                      'stimulus',
+                      'baseline',
+                      'stimulus',
+                      'baseline',
+                      'stimulus',
+                      'baseline',
+                      'stimulus',
+                      'baseline',
+                      'stimulus']}
+    sm_df = pd.DataFrame(scenemem_data, columns = ['onset', 'duration','trial_type'])
+    scenemem = {
+        'name': 'task-scenemem_events.tsv',
+        'data': sm_df.to_csv(index=False, sep='\t'),
+        'type': 'text/tab-separated-values'
+    }
+
+    sentence_data = {'onset': [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240],
+                     'duration': [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20],
+                     'weight': [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                     'trial_type':
+                     ['baseline',
+                      'stimulus',
+                      'baseline',
+                      'stimulus',
+                      'baseline',
+                      'stimulus',
+                      'baseline',
+                      'stimulus',
+                      'baseline',
+                      'stimulus',
+                      'baseline',
+                      'stimulus',
+                      'baseline']}
+    s_df = pd.DataFrame(sentence_data, columns = ['onset', 'duration','trial_type'])
+    sentence = {
+        'name': 'task-sentence_events.tsv',
+        'data': s_df.to_csv(index=False, sep='\t'),
+        'type': 'text/tab-separated-values'
+    }
+
+    wordgen_data = {'onset': [0, 30, 60, 90, 120, 150, 180, 210, 240, 270],
+                    'duration': [30, 30, 30, 30, 30, 30, 30, 30, 30, 30],
+                    'weight': [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+                    'trial_type':
+                   ['baseline',
+                    'stimulus',
+                    'baseline',
+                    'stimulus',
+                    'baseline',
+                    'stimulus',
+                    'baseline',
+                    'stimulus',
+                    'baseline',
+                    'stimulus']}
+    w_df = pd.DataFrame(wordgen_data, columns = ['onset', 'duration','trial_type'])
+    wordgen = {
+        'name': 'task-wordgen_events.tsv',
+        'data': w_df.to_csv(index=False, sep='\t'),
+        'type': 'text/tab-separated-values'
+    }
+    return [object, rhyme, scenemem, sentence, wordgen]
+
 
 MetadataExtras = {
    b0_phase: {
@@ -172,47 +314,72 @@ MetadataExtras = {
    asl: {
    "PulseSequenceType": "3D_SPIRAL",
        "PulseSequenceDetails" : "WIP" ,
+       "RepetitionTime":4.2,
        "LabelingType": "PCASL",
        "LabelingDuration": 1.8,
        "PostLabelingDelay": 1.8,
        "BackgroundSuppression": True,
        "BackgroundSuppressionNumberPulses": 2,
-       "M0":10,
-       "LabelingSlabLocation":"X",
+       "M0scale":10,
        "LabelingOrientation":"transversal",
        "LabelingDistance":105,
-       "AverageLabelingGradient": 10,
-       "SliceSelectiveLabelingGradient":45,
-       "AverageB1LabelingPulses": 0,
-       "AcquisitionDuration":123,
+       "LabelingPulseAverageGradient": 10,
+       "LabelingPulseMaximumGradient": 80,
        "VascularCrushing": False,
        "PulseDuration": 0.0005,
-       "InterPulseSpacing":4,
+       "LabelingPulseInterval": 0.00038,
        "PCASLType":"unbalanced",
-       "LabelingEfficiency":0.72}
+       "LabelingEfficiency":0.72},
+     binder_run1: {
+     "TaskName": "Binder Semantic Decision"},
+     binder_run2: {
+     "TaskName": "Binder Semantic Decision"},
+     object_run1: {
+     "TaskName": "Object Naming"},
+     object_run2: {
+     "TaskName": "Object Naming"},
+     rhyme_run1: {
+     "TaskName": "Rhyme Matching"},
+     rhyme_run2: {
+     "TaskName": "Rhyme Matching"},
+     scenemem_run1: {
+     "TaskName": "Scene Memory"},
+     scenemem_run2: {
+     "TaskName": "Scene Memory"},
+     sentence_run1: {
+     "TaskName": "Sentence Completion"},
+     sentence_run2: {
+     "TaskName": "Sentence Completion"},
+     verbgen_run1: {
+     "TaskName": "Verb Generation"},
+     verbgen_run2: {
+     "TaskName": "Verb Generation"},
+     wordgen_run1: {
+     "TaskName": "Word Generation"},
+     wordgen_run2: {
+     "TaskName": "Word Generation"},
 }
 
 IntendedFor = {
     b0_phase: [
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-object_run-01_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-object_run-02_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-rhyme_run-01_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-rhyme_run-02_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-scenemem_run-01_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-scenemem_run-02_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-sentence_run-01_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-sentence_run-02_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-wordgen_run-01_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-wordgen_run-02_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-binder_run-01_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-binder_run-02_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-verbgen_run-01_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-verbgen_run-02_bold.nii.gz',
-    'ses-{session}/func/sub-{subject}_ses-{session}_task-rest_bold.nii.gz'],
+    '{session}/func/sub-{subject}_{session}_task-object_run-01_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-object_run-02_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-rhyme_run-01_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-rhyme_run-02_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-scenemem_run-01_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-scenemem_run-02_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-sentence_run-01_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-sentence_run-02_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-wordgen_run-01_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-wordgen_run-02_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-binder_run-01_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-binder_run-02_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-verbgen_run-01_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-verbgen_run-02_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-rest_run-01_bold.nii.gz',
+    '{session}/func/sub-{subject}_{session}_task-rest_run-02_bold.nii.gz'],
 
-    m0: [ 'sub-{subject}/ses-{session}/perf/sub-{subject}_ses-{session}_asl.nii.gz' ],
+    m0: ['{session}/perf/sub-{subject}_{session}_asl.nii.gz'],
 
-    pe_rev: [
-        '{session}/dwi/sub-{subject}_{session}_acq-multiband_dwi.nii.gz',
-    ]
+    pe_rev: ['{session}/fmap/sub-{subject}_{session}_acq-multishell_dir-j_epi.nii.gz']
 }
